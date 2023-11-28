d728387e8f59ca27de3acc734e2b16304bcfd52e
mac <mac@macs-MacBook-Pro.local>
2023-11-28 12:40:40 +0530

    block2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // app.use(cors());
-app.use("/api/block", (req, res) => {
+app.use("/", (req, res) => {
     res.status(200).json({ message: "Busted" ,code:"1"});
   });
 
