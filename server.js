d6b17048d3b4df15f3fde65d04e1ef65fdbe69f2
mac <mac@macs-MacBook-Pro.local>
2023-11-28 11:22:37 +0530

    block

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use(cors());
+app.use("/api/block", (req, res) => {
+    res.status(200).json({ message: "Busted" ,code:"1"});
+  });
+
+
+app.all("*", (req, res) => {
+    res.status(404);
+  
+    res.sendFile("views/404.html", { root: __dirname });
+    throw new Error("Route Not Found");
+  });
+
+  const port =4000;
+  app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+  });
